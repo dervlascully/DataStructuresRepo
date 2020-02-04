d02fb1e54a358a278bde71f37398e7a8df13cfe5
dervlascully <48152283+dervlascully@users.noreply.github.com>
2020-02-04 11:34:22 +0000

    Update List.java

--- src/projectCode20280/List.java
+++ src/projectCode20280/List.java
@@ -12,21 +12,25 @@ import java.util.Iterator;
 
 public interface List<E> extends Iterable<E> {
 	
-	int size();
+	int getSize();
+// 	int size();
 	
 	boolean isEmpty();
 	
 	E get(int i);
 	
-	void add(int i, E e);
+	void add(E e, int i);
+// 	void add(int i, E e);
 	
 	void addFirst(E e);
 	void addLast(E e);
 	
 	E remove(int i);
 
-	E removeFirst();
-	E removeLast();
+// 	E removeFirst();
+// 	E removeLast();
+	void removeFirst();
+	void removeLast();
 	
 	Iterator<E> iterator();
 }
